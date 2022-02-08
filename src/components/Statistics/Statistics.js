23687c7347a54217a4018e24a1d13894a210123b
in-love-with-anime <98983660+fragile-chamomile@users.noreply.github.com>
2022-02-08 15:13:52 +0200

    fix2
    Этот заголовок не обязательный и его рендерим по условию

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 function Statistics({title, stats}) {
     return (
         <StatisticsWrapper>
-            <Title>{title}</Title>
+            {title && <Title>{title}</Title>}
 
             <Stats>
                 {stats.map(({id, label, percentage}) =>
